e1d7eadb75952bdd8e8ca4b6ea137ad54361b328
Godsleeper <nxtw@qq.com>
2016-10-01 23:19:47 +0800

    bug

--- js/game.js
+++ js/game.js
@@ -20,7 +20,7 @@ $(function(){
 	//遮罩
 	~function beforeStart(){
 		$("body").height(winHeight+"px");
-		$("body").click(()=>{	
+		$("body").click(function(){	
 			$(".mask").css({
 				display:"none"
 			});
