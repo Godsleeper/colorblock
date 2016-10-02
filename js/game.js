2619fee2c9573f163682b9ded360dc40a9ad6d77
Godsleeper <nxtw@qq.com>
2016-10-02 13:29:42 +0800

    testclick

--- js/game.js
+++ js/game.js
@@ -150,7 +150,7 @@ $(function(){
 						ctx.drawImage(this.bgimg,j*cell,i*cell,this.bgimg.width,this.bgimg.height);
 					}
 				}
-				$(".main").tap((event)=>{
+				$(".main").click((event)=>{
 					var x=event.pageX;
 					if(x>=0&&x<=clickbound*1){
 						that.setSite(-1)
