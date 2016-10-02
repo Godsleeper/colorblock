5c69acd36edd85e5965a84c173ad36cca7cf7a24
Godsleeper <nxtw@qq.com>
2016-10-02 20:09:23 +0800

    俄罗斯方块版本

--- js/game.js
+++ js/game.js
@@ -155,7 +155,7 @@ $(function(){
 		var background={
 			bgimg:imgObjs["bgpx.png"],
 			blockimg:[imgObjs["block_1.png"],imgObjs["block_2.png"],imgObjs["block_3.png"],imgObjs["block_4.png"]],
-			block:new block(1),
+			block:new block(4),
 			matrix:new Array(21),
 			score:0,//分数
 			next:next,
@@ -173,8 +173,7 @@ $(function(){
 					if(x>=0&&x<=clickbound*1){
 						that.setSite(-1)
 					}else if(x>1*clickbound&&x<=2*clickbound){
-						//that.rotate();
-						console.log("hello");
+						that.rotateBlock();
 					}else{
 						that.setSite(1);}
 						return false;
@@ -192,7 +191,6 @@ $(function(){
 					this.block.dropBlock();//下落
 					this.refresh();//刷新矩阵
 					this.reachBottom();//检测是否碰到地板或有方块
-
 				},config.TIME);
 			},
 			refresh:function(){
@@ -200,7 +198,7 @@ $(function(){
 				var that=this;
 				//矩阵走过的位置都置回-1
 				that.block.shame.forEach((item)=>{
-					if(item.i>0&&that.matrix[item.i-1][item.j]!=1){
+					if(item.i>0&&(that.matrix[item.i-1][item.j]!=1||that.matrix[item.i-1][item.j]!=2||that.matrix[item.i-1][item.j]!=3||that.matrix[item.i-1][item.j]!=4)){
 						that.matrix[item.i-1][item.j]=-1;
 					}
 				});
@@ -244,7 +242,7 @@ $(function(){
 				var length=this.block.shame.length;
 				for(i=0;i<length;i++){
 					shame=this.block.shame[i];
-					if(shame.i>=0 && ((background.matrix[shame.i][shame.j+dir]==1) || (shame.j+dir==-1 || shame.j + dir ==12))){
+					if(shame.i>=0 && ((background.matrix[shame.i][shame.j+dir]==1)||(background.matrix[shame.i][shame.j+dir]==2)||(background.matrix[shame.i][shame.j+dir]==3)||(background.matrix[shame.i][shame.j+dir]==4) || (shame.j+dir==-1 || shame.j + dir ==12))){
 						break;
 					}
 				}
@@ -293,6 +291,7 @@ $(function(){
 								return;
 							}
 						}
+						that.ruinMat();
 						that.block = new block(parseInt(Math.random()*5)+1);
 					}
 				}
@@ -304,7 +303,92 @@ $(function(){
 					display:"block"
 				})
 			},
-
+			rotatePoint: function (c, p,color) { // c点为旋转中心，p为旋转点，一次顺时针旋转90度。返回旋转后的坐标
+                return {j: p.i - c.i + c.j, i: -p.j + c.i + c.j,color:color};
+            },
+			rotateBlock:function(){
+				 var that = this, i, o = null, ctr = that.block.shame[1], l = that.block.shame.length;
+				 if (that.block.type != 4) { 
+				 for (i = 0; i < l; i++) {
+                        o = that.rotatePoint(ctr, that.block.shame[i],that.block.shame[i].color);
+                        if (o.j < 0 || o.j > 11 || o.i > 20) { // 旋转时不可以碰到边界
+                            break;
+                        }
+                        else if (o.i > 0 && o.j >= 0 && o.j <= 20 && ((background.matrix[o.i][o.j] == 1)||( background.matrix[o.i][o.j] == 2)||( background.matrix[o.i][o.j] == 3)|| (background.matrix[o.i][o.j] == 4))) { // 旋转时不可以已有方块的点
+                            break;
+                        }
+                    }
+                if (i == 4) {
+                        that.block.shame.forEach(function (o, i){
+                            if (o.i >= 0)
+                                that.matrix[o.i][o.j] = -1; // 清空变化前的位置
+                            that.block.shame[i] = that.rotatePoint(ctr, o,o.color);
+                        });
+                    }
+                }else{
+                	for(i=0;i<l;i++){
+                		if(that.block.shame[i].j<0||that.block.shame[i].j>11||that.block.shame[i].i>20){
+                			break;
+                		}else if(that.block.shame[i].i>0&&that.block.shame[i].j>=0&&that.block.shame[i].j<=20&&((background.matrix[that.block.shame[i].i][that.block.shame[i].j]==1)||(background.matrix[that.block.shame[i].i][that.block.shame[i].j]==2)||(background.matrix[that.block.shame[i].i][that.block.shame[i].j]==3)||(background.matrix[that.block.shame[i].i][that.block.shame[i].j]==4))){
+                			break;
+                		}
+                	}
+                	if (i == 4) {
+                        that.block.shame.forEach((o,i)=>{
+                            if (o.i >= 0)
+                                that.matrix[o.i][o.j] = -1; // 清空变化前的位置
+                        });
+                     var _color=that.block.shame[0].color;
+            		 that.block.shame[0].color=that.block.shame[1].color;
+            		 that.block.shame[1].color=that.block.shame[2].color;
+            		 that.block.shame[2].color=that.block.shame[3].color;
+            		 that.block.shame[3].color=_color;
+                    }
+                }
+			},
+			// detectTree:function(){
+			// 	//横=>层遍历
+			// 	console.log(background.matrix);
+			// 	for(let i=0;i<background.matrix.length;i++){
+			// 		var strArr=background.matrix[i].join("");
+			// 		strArr=strArr.replace(/(1{3})|(2{3})|(3{3})|(4{3})/g,"000");
+			// 		strArr=strArr.split("");
+			// 		for(let j=0;j<background.matrix[i].length;j++){
+			// 			background.matrix[i][j]=strArr[j];
+			// 		}
+			// 	}
+			// 	// background.matrix.forEach((item,index)=>{
+			// 	// 	var strArr=item.join("");
+			// 	// 	strArr=strArr.replace(/(1{3})|(2{3})|(3{3})|(4{3})/g,"000");
+			// 	// 	item=strArr.split("");
+			// 	// })
+			// },
+			// ruinBlock:function(){
+			// }
+			detectMat: function () { // 检测矩阵，判断是否有连续一行，返回一个数组
+                var count = 0, s,
+                    detecta = []; // 需要爆破的行号
+                this.matrix.forEach(function (l, i) {
+                    for(s = 0; s < l.length; s ++){
+                        if(l[s] == 1||l[s] == 2||l[s] == 3||l[s] == 4) count ++; else break;
+                    }
+                    count == 12 && detecta.push(i);
+                    count = 0;
+                });
+                return detecta.length == 0 ? false : detecta;
+            },
+            ruinMat: function () { // 爆破连续的一行
+                var dmat = this.detectMat(); // 返回整行都有方块的行号集合
+                if(dmat){
+                    this.score = this.score + (dmat.length == 1 ? 100 : dmat.length == 2 ? 250 : dmat.length == 3 ? 450 : 700);
+                    $(".score").innerHTML = this.score.toString();
+                    dmat.forEach(function (d) {
+                        background.matrix.splice(d, 1); // 删掉整行都有方块的行
+                        background.matrix.unshift([-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]); // 弥补被删的行
+                    });
+                }
+                dmat = null;
+            },
 		};	
 		background.init();
 		background.start();
